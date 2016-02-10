19b1705e3bf9bd5199b1d9bb53d030b1c34723f1
Filippos Vasilakis <vasilakisfil@gmail.com>
2016-02-10 11:46:44 +0100

    Minor fixes/improvements

--- lib/namespaced_mixin.rb
+++ lib/namespaced_mixin.rb
@@ -3,9 +3,9 @@ require "namespaced_mixin/version"
 module NamespacedMixin
   module ClassMethods
     def namespace(name, as:)
-      namespace = as
+      namespace = as.to_s
 
-      namespaced_class = name.split('::').inject(Object) {|o,c| o.const_get c}
+      namespaced_class = name.to_s.split('::').inject(Object) {|o,c| o.const_get c}
       cls = Class.new{
         include namespaced_class
       }.new
